cde27b75e2c15a304c082fa6d00cb794ae30901c
Ali Chopani <58507994+ach1992@users.noreply.github.com>
2025-07-14 01:01:58 +0330

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Bot token
+# گرفتن توکن ربات از کاربر و ذخیره آن در متغیر tk
 while [[ -z "$tk" ]]; do
     echo "Bot token: "
     read -r tk
@@ -9,7 +11,8 @@ while [[ -z "$tk" ]]; do
     fi
 done
 
-
+# Chat id
+# گرفتن Chat ID از کاربر و ذخیره آن در متغیر chatid
 while [[ -z "$chatid" ]]; do
     echo "Chat id: "
     read -r chatid
@@ -22,16 +25,18 @@ while [[ -z "$chatid" ]]; do
     fi
 done
 
-
+# Caption
+# گرفتن عنوان برای فایل پشتیبان و ذخیره آن در متغیر caption
 echo "Caption (for example, your domain, to identify the database file more easily): "
 read -r caption
 
-
+# Cronjob
+# تعیین زمانی برای اجرای این اسکریپت به صورت دوره‌ای
 while true; do
     echo "Cronjob (minutes and hours) (e.g : 30 6 or 0 12) : "
     read -r minute hour
     if [[ $minute == 0 ]] && [[ $hour == 0 ]]; then
-        cron_time="0 0 * * *"
+        cron_time="* * * * *"
         break
     elif [[ $minute == 0 ]] && [[ $hour =~ ^[0-9]+$ ]] && [[ $hour -lt 24 ]]; then
         cron_time="0 */${hour} * * *"
@@ -48,16 +53,17 @@ while true; do
 done
 
 
-
-while [[ -z "$xmhs" ]]; do
-    echo "x-ui or s-ui or marzban or hiddify? [x/s/m/h] : "
-    read -r xmhs
-    if [[ $xmhs == $'\0' ]]; then
-        echo "Invalid input. Please choose x,s, m or h."
-        unset xmhs
-    elif [[ ! $xmhs =~ ^[xmhs]$ ]]; then
-        echo "${xmhs} is not a valid option. Please choose x, m or h."
-        unset xmhs
+# x-ui or marzban or hiddify
+# گرفتن نوع نرم افزاری که می‌خواهیم پشتیبانی از آن بگیریم و ذخیره آن در متغیر xmh
+while [[ -z "$xmh" ]]; do
+    echo "x-ui or marzban or hiddify? [x/m/h] : "
+    read -r xmh
+    if [[ $xmh == $'\0' ]]; then
+        echo "Invalid input. Please choose x, m or h."
+        unset xmh
+    elif [[ ! $xmh =~ ^[xmh]$ ]]; then
+        echo "${xmh} is not a valid option. Please choose x, m or h."
+        unset xmh
     fi
 done
 
@@ -74,11 +80,14 @@ while [[ -z "$crontabs" ]]; do
 done
 
 if [[ "$crontabs" == "y" ]]; then
+# remove cronjobs
 sudo crontab -l | grep -vE '/root/ac-backup.+\.sh' | crontab -
 fi
 
 
-if [[ "$xmhs" == "m" ]]; then
+# m backup
+# ساخت فایل پشتیبانی برای نرم‌افزار Marzban و ذخیره آن در فایل ac-backup.zip
+if [[ "$xmh" == "m" ]]; then
 
 if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
   echo "The folder exists at $dir"
@@ -87,25 +96,14 @@ else
   exit 1
 fi
 
-if [ -d "/var/lib/marzban/mysql" ] || [ -d "/var/lib/mysql/marzban" ]; then
-
-path=""
-
 if [ -d "/var/lib/marzban/mysql" ]; then
-  path="/var/lib/marzban/mysql"
-elif [ -d "var/lib/mysql/marzban" ]; then
-  path="var/lib/mysql/marzban"
-else
-  echo "Neither path exists."
-  exit 1
-fi
 
   sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
 
   docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
   source /opt/marzban/.env
 
-    cat > "$path/ac-backup.sh" <<EOL
+    cat > "/var/lib/marzban/mysql/ac-backup.sh" <<EOL
 #!/bin/bash
 
 USER="root"
@@ -117,21 +115,19 @@ databases=\$(mysql -h 127.0.0.1 --user=\$USER --password=\$PASSWORD -e "SHOW DAT
 for db in \$databases; do
     if [[ "\$db" != "information_schema" ]] && [[ "\$db" != "mysql" ]] && [[ "\$db" != "performance_schema" ]] && [[ "\$db" != "sys" ]] ; then
         echo "Dumping database: \$db"
-		mysqldump -h 127.0.0.1 --force --opt --user=\$USER --password=\$PASSWORD  --routines --databases \$db > /var/lib/mysql/db-backup/\$db.sql
+		mysqldump -h 127.0.0.1 --force --opt --user=\$USER --password=\$PASSWORD --databases \$db > /var/lib/mysql/db-backup/\$db.sql
 
     fi
 done
 
 EOL
-chmod +x "$path/ac-backup.sh"
+chmod +x /var/lib/marzban/mysql/ac-backup.sh
 
 ZIP=$(cat <<EOF
-#!/bin/bash
-
 docker exec marzban-mysql-1 bash -c "/var/lib/mysql/ac-backup.sh"
-zip -r /root/ac-backup-m.zip /opt/marzban/* /var/lib/marzban/* /opt/marzban/.env -x "$path/\*"
-zip -r /root/ac-backup-m.zip "$path/db-backup/*"
-rm -rf "$path/db-backup/*"
+zip -r /root/ac-backup-m.zip /opt/marzban/* /var/lib/marzban/* /opt/marzban/.env -x /var/lib/marzban/mysql/\*
+zip -r /root/ac-backup-m.zip /var/lib/marzban/mysql/db-backup/*
+rm -rf /var/lib/marzban/mysql/db-backup/*
 EOF
 )
 
@@ -139,76 +135,55 @@ EOF
       ZIP="zip -r /root/ac-backup-m.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
 fi
 
-ACh="marzban backup"
+ACh1992="marzban backup"
 
-elif [[ "$xmhs" == "x" || "$xmhs" == "s" ]]; then
+# x-ui backup
+# ساخت فایل پشتیبانی برای نرم‌افزار X-UI و ذخیره آن در فایل ac-backup.zip
+elif [[ "$xmh" == "x" ]]; then
 
-ACh=""
-dbDir=$(find /etc /opt/freedom /usr/local \
-           -type d \( -iname "x-ui*" -o -iname "s-ui" \) \
-           -print -quit 2>/dev/null)
-
-
-if [[ -n "${dbDir}" ]]; then
+if dbDir=$(find /etc /opt/freedom -type d -iname "x-ui*" -print -quit); then
   echo "The folder exists at $dbDir"
-  if [[ $dbDir == "/opt/freedom/x-ui"* ]]; then
-    dbDir="${dbDir}/db/x-ui.db"
-    ACh="x-ui backup"
-  elif [[ $dbDir == "/usr/local/s-ui" ]]; then
-    dbDir="${dbDir}/db/s-ui.db" 
-    ACh="s-ui backup"
-  else
-    dbDir="${dbDir}/x-ui.db"
-    ACh="x-ui backup"
+  if [[ $dbDir == *"/opt/freedom/x-ui"* ]]; then
+     dbDir="${dbDir}/db/"
   fi
 else
   echo "The folder does not exist."
   exit 1
 fi
 
-configDir=$(find /usr/local -type d -iname "x-ui*" -print -quit 2>/dev/null)
-if [[ -n "${configDir}" ]]; then
+if configDir=$(find /usr/local -type d -iname "x-ui*" -print -quit); then
   echo "The folder exists at $configDir"
-  configDir="${configDir}/config.json"
 else
-  configDir=""
-fi
-
-ZIP="zip /root/ac-backup-${xmhs}.zip ${dbDir} ${configDir}"
-
-
-elif [[ "$xmhs" == "h" ]]; then
-
-if ! find /opt/hiddify-manager/hiddify-panel/ -type d -iname "backup" -print -quit; then
   echo "The folder does not exist."
   exit 1
 fi
 
-if [ -f "/opt/hiddify-manager/hiddify-panel/backup.sh" ]; then
-  backupCommand="bash backup.sh"
-else
-backupCommand="python3 -m hiddifypanel backup"
-fi
+ZIP="zip /root/ac-backup-x.zip ${dbDir}/x-ui.db ${configDir}/config.json /root/*.key /root/*.crt"
+ACh1992="x-ui backup"
 
+# hiddify backup
+# ساخت فایل پشتیبانی برای نرم‌افزار Hiddify و ذخیره آن در فایل ac-backup.zip
+elif [[ "$xmh" == "h" ]]; then
+
+if ! find /opt/hiddify-config/hiddify-panel/ -type d -iname "backup" -print -quit; then
+  echo "The folder does not exist."
+  exit 1
+fi
 
 ZIP=$(cat <<EOF
-#!/bin/bash
-
-cd /opt/hiddify-manager/hiddify-panel/
-if [ $(find /opt/hiddify-manager/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
-  find /opt/hiddify-manager/hiddify-panel/backup -type f -delete
+cd /opt/hiddify-config/hiddify-panel/
+if [ $(find /opt/hiddify-config/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
+  find /opt/hiddify-config/hiddify-panel/backup -type f -delete
 fi
-
-$backupCommand
-
-cd /opt/hiddify-manager/hiddify-panel/backup
+python3 -m hiddifypanel backup
+cd /opt/hiddify-config/hiddify-panel/backup
 latest_file=\$(ls -t *.json | head -n1)
 rm -f /root/ac-backup-h.zip
-zip /root/ac-backup-h.zip /opt/hiddify-manager/hiddify-panel/backup/\$latest_file
+zip /root/ac-backup-h.zip /opt/hiddify-config/hiddify-panel/backup/\$latest_file
 
 EOF
 )
-ACh="hiddify backup"
+ACh1992="hiddify backup"
 else
 echo "Please choose m or x or h only !"
 exit 1
@@ -216,32 +191,42 @@ fi
 
 
 trim() {
+    # remove leading and trailing whitespace/lines
     local var="$*"
+    # remove leading whitespace characters
     var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
     var="${var%"${var##*[![:space:]]}"}"
     echo -n "$var"
 }
 
 IP=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
-caption="${caption}\n\n${ACh}\n<code>${IP}</code>\nCreated by @ACh1992 - https://github.com/ach1992"
+caption="${caption}\n\n${ACh1992}\n<code>${IP}</code>\nCreated by @ACh1992 - https://github.com/ach1992"
 comment=$(echo -e "$caption" | sed 's/<code>//g;s/<\/code>//g')
 comment=$(trim "$comment")
 
-
+# install zip
+# نصب پکیج zip
 sudo apt install zip -y
 
-cat > "/root/ac-backup-${xmhs}.sh" <<EOL
-rm -rf /root/ac-backup-${xmhs}.zip
+# send backup to telegram
+# ارسال فایل پشتیبانی به تلگرام
+cat > "/root/ac-backup-${xmh}.sh" <<EOL
+rm -rf /root/ac-backup-${xmh}.zip
 $ZIP
-echo -e "$comment" | zip -z /root/ac-backup-${xmhs}.zip
-curl -F chat_id="${chatid}" -F caption=\$'${caption}' -F parse_mode="HTML" -F document=@"/root/ac-backup-${xmhs}.zip" https://api.telegram.org/bot${tk}/sendDocument
+echo -e "$comment" | zip -z /root/ac-backup-${xmh}.zip
+curl -F chat_id="${chatid}" -F caption=\$'${caption}' -F parse_mode="HTML" -F document=@"/root/ac-backup-${xmh}.zip" https://api.telegram.org/bot${tk}/sendDocument
 EOL
 
 
+# Add cronjob
+# افزودن کرانجاب جدید برای اجرای دوره‌ای این اسکریپت
+{ crontab -l -u root; echo "${cron_time} /bin/bash /root/ac-backup-${xmh}.sh >/dev/null 2>&1"; } | crontab -u root -
 
-{ crontab -l -u root; echo "${cron_time} /bin/bash /root/ac-backup-${xmhs}.sh >/dev/null 2>&1"; } | crontab -u root -
+# run the script
+# اجرای این اسکریپت
+bash "/root/ac-backup-${xmh}.sh"
 
-
-bash "/root/ac-backup-${xmhs}.sh"
-
+# Done
+# پایان اجرای اسکریپت
 echo -e "\nDone\n"
